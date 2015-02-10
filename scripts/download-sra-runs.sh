d5b1c35d4d94ec27e4b3a871cdd9792f5a50e068
Robert A Petit III <robert.petit@emory.edu>
2015-02-09 16:32:22 -0800

    script uses prefetch to download SRP051511 runs

--- scripts/download-sra-runs.sh
+++ scripts/download-sra-runs.sh
@@ -1,1 +1,6 @@
-refetch -t ascp -a "/data1/home/rpetit/.aspera/connect/bin/ascp|/data1/home/rpetit/.aspera/connect/etc/asperaweb_id_dsa.openssh" --option-file data/SRP051511.txt
+#! /bin/bash
+
+mkdir sra-runs
+cd sra-runs
+prefetch -t ascp -a "/data1/home/rpetit/.aspera/connect/bin/ascp|/data1/home/rpetit/.aspera/connect/etc/asperaweb_id_dsa.openssh" --option-file ../data/SRP051511.txt 1> ../prefetch.out 2> ../prefetch.err
+cd ..
