7122ab9118528f76bac49300c2a32440c36b35ae
Robert A Petit III <robert.petit@emory.edu>
2015-02-11 09:32:35 -0800

    Forget giving prefetch a full list, download one at a time

--- scripts/download-sra-runs.sh
+++ scripts/download-sra-runs.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-mkdir sra-runs
-cd sra-runs
-prefetch -t ascp -a "/data1/home/rpetit/.aspera/connect/bin/ascp|/data1/home/rpetit/.aspera/connect/etc/asperaweb_id_dsa.openssh" --option-file ../data/SRP051511.txt 1> ../prefetch.out 2> ../prefetch.err
-cd ..
+while read line
+do
+
+prefetch -a "/data1/home/rpetit/.aspera/connect/bin/ascp|/data1/home/rpetit/.aspera/connect/etc/asperaweb_id_dsa.openssh" $line
+
+done < $1
